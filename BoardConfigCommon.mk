ac4fa79a7f9c27b3b3c7ab839ad4e026a10d8583
Rajeev Kumar <rajekumar@google.com>
2018-03-12 15:26:29 -0700

    tama-common: Enable memory stats logging in user mode LMKD
    Bug: 74443701
    Test: Tested manually
    Merged-In: I288098e17d5fc65923758152bba5e786a75edc4c
    Change-Id: I288098e17d5fc65923758152bba5e786a75edc4c
    (cherry picked from commit 56395c15a57eda7aa59a7615422677d5a8075350)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
+# LMKD
+TARGET_LMKD_STATS_LOG := true
+
 # Media
 TARGET_USES_ION := true
 
